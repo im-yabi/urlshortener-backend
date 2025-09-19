8a02c2e54d427e5f5b908bd6a0303b3c024a17ae
im-yabi <151531028+im-yabi@users.noreply.github.com>
2025-09-19 15:10:02 +0530

    ..

--- server.js
+++ server.js
@@ -27,6 +27,5 @@ app.use((err, req, res, next) => {
   });
 });
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`🚀 Server running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
